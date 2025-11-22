2bd45cebe4a557e2f7094f0fb1a0c8acab774562
Neelambari <neelambari@Neelambaris-Mac-mini.local>
2025-11-22 17:13:48 -0500

    feat(add): longest consective sequence

--- js/test.js
+++ js/test.js
@@ -1,3 +1,26 @@
-const array = [1, 2, 3, 4];
+var longestConsecutive = function(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
 
-console.log(array.filter((item) => item !== array[1]));
+    const numSet = new Set(nums);
+    let longestStreak = 0;
+
+    for (const num of numSet) {
+        // Check if 'num' is the start of a sequence
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            // Extend the sequence as long as consecutive numbers exist
+            while (numSet.has(currentNum + 1)) {
+                currentNum++;
+                currentStreak++;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+};
