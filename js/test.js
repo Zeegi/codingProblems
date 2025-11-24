06f297424d30795948d281d2193b0d52ef19ab71
Neelambari <neelambari@Neelambaris-Mac-mini.local>
2025-11-24 12:34:19 -0500

    feat(add): folder for python excercise

--- js/test.js
+++ js/test.js
@@ -1,33 +1,16 @@
 
-const randomNums = [23, 1, 34, 3, 2,22, 24, 35, 25, 26, 27, 28, 29, 4, 5 ];
+const randomString = "hariprasad";
 
-const longestConsecutive = function(nums) {
-    if (nums.length === 0) {
-        return 0;
+
+const reverseAString = (randomString) => {
+
+    const stringArrays = randomString.split("");
+    
+    for(i= stringArrays.length - 1; i >= 0; i--){
+
+        console.log("reversing", randomString[i]);
     }
 
-    const numSet = new Set(nums);
-
-    console.log("numSet", numSet);
-    let longestStreak = 0;
-
-    for (const num of numSet) {
-        // Check if 'num' is the start of a sequence
-        if (!numSet.has(num - 1)) {
-            let currentNum = num;
-            let currentStreak = 1;
-
-            // Extend the sequence as long as consecutive numbers exist
-            while (numSet.has(currentNum + 1)) {
-                currentNum++;
-                currentStreak++;
-            }
-
-            longestStreak = Math.max(longestStreak, currentStreak);
-        }
-    }
-
-    return longestStreak;
 };
 
-console.log(longestConsecutive(randomNums));
+console.log(reverseAString(randomString));
