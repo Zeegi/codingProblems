2adaf9bf1f97a4a59e1e9ed7b3b90a7e63103f12
Neelambari <neelambari@Neelambaris-Mac-mini.local>
2025-11-23 20:19:08 -0500

    feat(add): add the longest consecutive number

---
+++ js/consecutiveLongestSequence.js
@@ -0,0 +1,29 @@
+const randomNums = [23, 1, 34, 3, 2, 24, 35, 25, 26, 27, 28, 29, 4, 5, 22];
+
+const longestConsectiveSequence = (nums) => {
+
+    if (nums.length < 1) {
+        return 0;
+    }
+
+    const numsSet = new Set(nums);
+    let longestStreak = 0;
+
+    for (const num of numsSet) {
+        // entering into the loop only if the prior number is NOT present in the list. If it is there it skips the loop. 
+        // this number which is skipped will be counted when the real prior number is been processed
+        if (!numsSet.has(num - 1)) {
+            let currentNumber = num;
+            currentStreak = 1;
+
+            while (numsSet.has(currentNumber + 1)) {
+                currentNumber++;
+                currentStreak++;
+            }
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+    return longestStreak;
+};
+
+console.log(longestConsectiveSequence(randomNums));
--- js/test.js
+++ js/test.js
@@ -1,9 +1,14 @@
-var longestConsecutive = function(nums) {
+
+const randomNums = [23, 1, 34, 3, 2,22, 24, 35, 25, 26, 27, 28, 29, 4, 5 ];
+
+const longestConsecutive = function(nums) {
     if (nums.length === 0) {
         return 0;
     }
 
     const numSet = new Set(nums);
+
+    console.log("numSet", numSet);
     let longestStreak = 0;
 
     for (const num of numSet) {
@@ -24,3 +29,5 @@ var longestConsecutive = function(nums) {
 
     return longestStreak;
 };
+
+console.log(longestConsecutive(randomNums));
