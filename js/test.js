b9eba7b0ab8b26ef0f1beb8cb04e44e0c64fab2c
Neelambari <neelambari@Neelambaris-Mac-mini.local>
2025-12-06 06:50:13 -0500

    feat(add): added

--- js/test.js
+++ js/test.js
@@ -1,2 +1,45 @@
 
 // testing the functionality of a simple addition function
+
+const array = [23, 1, 2221, 34, 90, 56, 12];
+
+// const testFunction = (parameter) => {
+//     let sum = 0
+//     for (i=0; i< parameter.length; i++){
+//         if(parameter[i]){
+//             sum = sum + parameter[i];
+//         }
+//     }
+//     return sum;
+// };
+
+// console.log(testFunction(array));
+
+
+// const testFunction = (parameter) => {
+//     let largest = parameter[0];
+//     for (i=1; i< parameter.length; i++){
+//         if(parameter[i] > largest){
+//             largest = parameter[i];
+//         }
+//     }
+//     return largest;
+// };
+
+// console.log(testFunction(array));
+
+const testFunction = (parameter) => {
+    let smallest = parameter[0];
+    for (i=1; i< parameter.length; i++){
+        if(parameter[i] < smallest){
+            smallest = parameter[i];
+        }
+    }
+    return smallest;
+};
+
+console.log(testFunction(array));
+
+//largest
+//smallest
+//sum
