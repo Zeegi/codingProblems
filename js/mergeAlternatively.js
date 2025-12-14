4375c11fb71f6a8d90bac7e0000de75ef23a97bf
Neelambari <neelambari@Neelambaris-Mac-mini.local>
2025-12-13 20:33:07 -0500

    feat(add): adding a problem

---
+++ js/mergeAlternatively.js
@@ -0,0 +1,39 @@
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+ 
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+
+
+const mergedAlternatively = (word1, word2) => {
+
+    if((word1.length || word2.length) <= 0){
+        return ''
+    }
+
+    let programLength;
+    let finalString = [];
+    programLength = word1.length > word2.length ? word1.length : word2.length;
+
+    for(i=0; i < programLength; i++){
+
+        word1Char = word1[i];
+        finalString.push(word1Char);
+        word2Char = word2[i];
+        finalString.push(word2Char);
+    }
+
+    console.log('finalString', finalString.join(""));
+
+}
+
+console.log(mergedAlternatively('Hariprasad', 'Neela'));
